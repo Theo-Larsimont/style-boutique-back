61c9fc25adf2a76a609fca7798c2310568212f70
Théo Larsimont <theolarsimont@proton.me>
2024-10-22 13:22:09 +0200

    initial commit

---
+++ src/test/java/org/example/styleboutiqueback/StyleBoutiqueBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.styleboutiqueback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyleBoutiqueBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
